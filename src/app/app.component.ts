121eeea64c45f4f86801ead59c872293ac4efb0f
Carolina Parraga <carol_parraga@hotmail.com>
2023-05-19 19:27:13 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from "./auth/services/auth.service";
 
 @Component({
   selector: 'rm-root',
@@ -13,4 +14,13 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'rent-motobike';
+  logged = false;
+
+  constructor(
+    public authService: AuthService,
+  ) {}
+
+  logout() {
+    this.authService.doLogout()
+  }
 }
--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
-import { User } from '../../user/interfaces/user';
+import { User } from '../interfaces/user'
 import { UserService } from '../services/user.service';
-
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
 
 @Component({
   selector: 'rm-user-profile',
   standalone: true,
   imports: [
-    CommonModule,
-    RouterLink,
-    RouterLinkActive,
-    RouterOutlet
+    CommonModule, RouterLink, RouterLinkActive, RouterOutlet
   ],
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
   user!: User;
+  userAdmin! : string [];
+  bool = false;
 
 
   constructor(
@@ -48,13 +45,24 @@ export class UserProfileComponent implements OnInit {
 
     this.userService.getProfile()
     .subscribe({
-      next: rta => this.user = rta,
+      next: rta => {
+        this.user = rta
+        console.log(this.user)
+        this.userAdmin = this.user.roles!
+        this.bool = this.userAdmin.includes('ROLE_ADMIN')
+        console.log(this.bool)
+      },
       error: error => console.error(error),
       complete: () => console.log("User loaded")
     });
 
+  }
+  role(){
+    this.userAdmin = this.user.roles!
+    this.bool = this.userAdmin.includes('ROLE_ADMIN')
 
   }
+
   goBack() {
     this.router.navigate(['/motos']);
   }
